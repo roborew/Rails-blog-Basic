9ee8c146039f145afc1c2ef6e2de58ced041d5f1
Robin Andrew <robinrew@gmail.com>
2024-08-27 15:29:48 +0100

    Update layout with new meta tags and links.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,7 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article), status: :see_other
   end
 
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
